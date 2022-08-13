e2531a28d30edc14f2d2a9f90caca185d9d15c97
TheBigMort <thebigmort1249@gmail.com>
2022-08-13 05:58:05 -0500

    fixes

--- dist/util/asyncHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/**
- * Catches errors and passes them to the next callback
- * @param handler Async express request handler/middleware potentially throwing errors
- * @returns Async express request handler with error handling
- */
-exports.default = (handler) => (req, res, next) => handler(req, res, next).catch(next);
